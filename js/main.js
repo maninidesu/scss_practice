6845b6556c03dd0f0a21c9bfd72a9143c2a8e6ab
maninidesu <maninisns@gmail.com>
2024-01-02 10:07:34 +0900

    いろんな小技があるネ

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-/* js01
+/* javascript
 ------------------------------------------ */
+/* js01 */
 const js01Btn = document.getElementById('js01Btn');
 js01Btn.addEventListener('click', function(){
     this.innerHTML = 'クリック済';
     this.style.backgroundColor = 'gray';
     this.setAttribute('disabled', true);
 });
-/* js02
------------------------------------------- */
+/* js02 */
 const js02BtnAdd = document.getElementById('js02Btn-add');
 const js02BtnRemove = document.getElementById('js02Btn-remove');
 const js02List = document.querySelector('.js02-list');
@@ -24,8 +24,7 @@ js02BtnRemove.addEventListener('click', function(){
         js02List.removeChild(js02List.lastChild);
     };
 });
-/* js03
------------------------------------------- */
+/* js03 */
 const js03List = document.querySelector('.js03-list');
 const js03Btn = document.querySelectorAll('.js03Btn-wrap .jsBtn');
 const js03BtnRemove = document.getElementById('js03Btn-remove');
@@ -43,8 +42,7 @@ for(let i = 0; i < js03Btn.length; i++){
 js03BtnRemove.addEventListener('click', function(){
     js03List.removeChild(js03List.lastElementChild);
 });
-/* js04
------------------------------------------- */
+/* js04 */
 const js04Btn = document.getElementsByClassName('js04Btn');
 
 for(let j = 0; j < js04Btn.length; j++){
@@ -53,7 +51,9 @@ for(let j = 0; j < js04Btn.length; j++){
         this.classList.toggle('js04Btn-clicked');
     });
 }
-/*
+/* tricks
 ------------------------------------------ */
+/* tricks03*/
+
 /*
 ------------------------------------------ */
